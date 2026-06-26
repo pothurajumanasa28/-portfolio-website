ff6d590eac4cdca70ea39e380d365bd6a20cdd3f
pothurajumanasa28 <pothurajumanasa28@gmail.com>
2026-06-26 12:47:41 +0530

    Connected frontend to Render backend

--- portfolio-frontend/src/components/Projects.js
+++ portfolio-frontend/src/components/Projects.js
@@ -6,7 +6,7 @@ function Projects() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/api/projects")
+      .get("https://portfolio-backend-0kzl.onrender.com/api/projects")
       .then((res) => {
         setProjects(res.data);
       })
@@ -16,28 +16,25 @@ function Projects() {
   }, []);
 
   return (
-  <section>
-    <h2>Projects</h2>
+    <section>
+      <h2>Projects</h2>
 
-    {projects.map((project) => (
-      <div
-  key={project._id}
-  style={{
-    border: "1px solid #ddd",
-    padding: "15px",
-    marginBottom: "10px",
-    borderRadius: "8px"
-  }}
->
-  <h3>{project.title}</h3>
-  <p>{project.description}</p>
-</div>
-        
-        
-    ))}
-  </section>
-);
-    
+      {projects.map((project) => (
+        <div
+          key={project._id}
+          style={{
+            border: "1px solid #ddd",
+            padding: "15px",
+            marginBottom: "10px",
+            borderRadius: "8px",
+          }}
+        >
+          <h3>{project.title}</h3>
+          <p>{project.description}</p>
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default Projects;
